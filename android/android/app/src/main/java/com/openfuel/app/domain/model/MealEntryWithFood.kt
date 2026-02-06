38ffca938f70c93eb7f37c1b231c2c485c052e25
AmpWatch <robertr.90@icloud.com>
2026-02-06 16:12:33 +0000

    Repo hygiene: remove nested android/android project

--- android/android/app/src/main/java/com/openfuel/app/domain/model/MealEntryWithFood.kt
+++
@@ -1,6 +0,0 @@
-package com.openfuel.app.domain.model
-
-data class MealEntryWithFood(
-    val entry: MealEntry,
-    val food: FoodItem,
-)
--- android/android/app/src/main/java/com/openfuel/app/domain/repository/SettingsRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.openfuel.app.domain.repository
-
-import kotlinx.coroutines.flow.Flow
-
-interface SettingsRepository {
-    val onlineLookupEnabled: Flow<Boolean>
-    suspend fun setOnlineLookupEnabled(enabled: Boolean)
-}
